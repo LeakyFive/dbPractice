28724c4c00eccca5b1490497e7c6b167c1b231b8
LeakyFive <braintosha@gmail.com>
2018-10-23 09:21:53 +0300

    Update dbClass.php

--- php/dbClass.php
+++ php/dbClass.php
@@ -2,9 +2,28 @@
 
 class DBConnection
 {
-	
+	protected $conn;
+
+	protected $defaults = [
+		'host' => 'localhost',
+		'user' => 'root',
+		'pass' => '',
+		'db' => 'dbName',
+		'charset' => 'utf8',
+	];
+
+	const $FETCH_MODE = MYSQLI_ASSOC;
+
 	function __construct()
 	{
-		
+		$opt = array_merge($this->defaults, $opt);
+		$this->conn = new musqli($opt['host'], $opt['user'], $opt['pass'], $opt['db']);
+		if (!$this->conn) exit('Lost DB connection');
+		$this->conn->set_charset($opt['charset']);
+	}
+
+	function fetch($result)
+	{
+		return $result->fetch_all($mode = self::FETCH_MODE)
 	}
 }
