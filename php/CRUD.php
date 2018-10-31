83afb1bd3e2589de35c40e5b8a2736f89b400674
LeakyFive <braintosha@gmail.com>
2018-10-31 14:59:12 +0300

    Добавлены комментарии

--- php/CRUD.php
+++ php/CRUD.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once "dbClass.php";
+require_once "dbClass.php"; //подключаем наш класс подключения к БД
 
-$conn = new ConnectDB();
-$operationType = $_POST['action'];
+$conn = new ConnectDB(); //создаём экземпляр класса
+$operationType = $_POST['action']; //получаем тип операции к БД полученный из суперглобального массива POST, тип операции храним на кнопке выполняющей ту или иную операцию
 
-if ($operationType == 'update') {
+if ($operationType == 'update') { //собираем данные для выполнения update
 	$table = $_POST['table'];
 	$id = $_POST['id'];
 	$types = $_POST['types'];
@@ -13,7 +13,7 @@ if ($operationType == 'update') {
 	$values = $_POST['values'];
 	$conn->makePreparedQuery($table, $operationType, $id, $types, $field, $values);
 }
-if ($operationType == 'insert') {
+if ($operationType == 'insert') { //собираем данные для выполнения inserts
 	$table = $_POST['table'];
 	$id = $_POST['id'];
 	$types = $_POST['types'];
@@ -21,7 +21,7 @@ if ($operationType == 'insert') {
 	$values = $_POST['values'];
 	$conn->makePreparedQuery(($table, $operationType, $id, $types, $field, $values);
 }
-if ($operationType == 'delete') {
+if ($operationType == 'delete') { //собираем данные для выполнения delete
 	$table = $_POST['table'];
 	$id = $_POST['id'];
 	$conn->makePreparedQuery($table, $id);
